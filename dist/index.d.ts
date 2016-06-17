6b48391f06ee53bfc0f2a57b28bfd8b076b369ff
Giovanni Lovato <heruan@aldu.net>
2016-06-17 13:06:07 +0200

    chore(package): rename to aurelia-push

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,1 @@
 export { NotificationManager } from "./notification-manager";
-export { BrowserNotificationManager } from "./browser-notification-manager";
--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,4 @@
 "use strict";
 var notification_manager_1 = require("./notification-manager");
 exports.NotificationManager = notification_manager_1.NotificationManager;
-var browser_notification_manager_1 = require("./browser-notification-manager");
-exports.BrowserNotificationManager = browser_notification_manager_1.BrowserNotificationManager;
 //# sourceMappingURL=index.js.map
