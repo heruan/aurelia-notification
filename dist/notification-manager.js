833ff4831455dcc8507b815354cf52bc98482ae8
Giovanni Lovato <heruan@aldu.net>
2016-06-17 12:25:26 +0200

    chore(package): add dist folder

---
+++ dist/notification-manager.js
@@ -0,0 +1,8 @@
+"use strict";
+var NotificationManager = (function () {
+    function NotificationManager() {
+    }
+    return NotificationManager;
+}());
+exports.NotificationManager = NotificationManager;
+//# sourceMappingURL=notification-manager.js.map
